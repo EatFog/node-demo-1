6cc982f319f4dd87bace0b108757a08fbc5b455f
EatFog <eatfog@qq.com>
2020-06-03 02:36:06 +0800

    上传第2次node.js

--- node.js
+++ node.js
@@ -24,7 +24,17 @@ var server = http.createServer(function(request, response){
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`二哈`)
+    response.write(`<!DOCTYPE html>
+    <html>
+    <head>
+      <title>nodetest</title>
+      <link rel="stylesheet" href="./x">
+    </head>
+    <body>
+      <h1>title</h1>
+    </body>
+    </html>
+    `)
     response.end()
   } else if(path === '/x'){
     response.statusCode = 200
@@ -34,7 +44,7 @@ var server = http.createServer(function(request, response){
   } else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`你输入的路径不存在对应的内容`)
+    response.write(response.statusCode+`:你访问的页面不存在`)
     response.end()
   }
 
